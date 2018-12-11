3ba8e4810cd3a88baa4aef4a2cefec2000281519
Amer <amer.malibu@gmail.com>
2018-12-12 00:31:43 +0100

    JavaScript Classes Methods

--- objects.js
+++ objects.js
@@ -3,12 +3,20 @@ class User {
         this.email = email;
         this.name = name;
     }
+
+    login(){
+        console.log(this.email,' just logged in');
+    }
+
+    logout(){
+        console.log(this.email, ' just logged out');
+    }
+    
 }
 
 var userOne = new User('amer@gmail.com','Amer');
 var userTwo = new User('hanad@hotmail.com','Hanad');
 
-console.log(userOne);
-console.log(userTwo);
-
+userOne.login();
+userTwo.logout();
 
